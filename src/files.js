27bceb2d7f1eb61b04e5e43cc51081d93ff20e2f
pstraw <github_main@pstrawberrie.live>
2025-07-17 12:44:01 -0500

    update pswg.tre hash

--- src/files.js
+++ src/files.js
@@ -7,7 +7,7 @@ export const directoryList = [
 ]
 
 export const fileList = [
-  { filePath: 'pswg.tre', fileHash: 'bfb81304dbce24f0383bd86f357737b5' },
+  { filePath: 'pswg.tre', fileHash: '0a76ac853e8b6ceb7c0ecd02865f03b1' },
   { filePath: 'default_patch.tre', fileHash: '907194cd54efd6820c84db37c47dfe2d' },
   { filePath: 'patch_sku1_14_00.tre', fileHash: '3170521a8e7e0547e9117bc092cbd021' },
   { filePath: 'patch_14_00.tre', fileHash: '0649bd5305dd18cd5378cf542647e543' },
